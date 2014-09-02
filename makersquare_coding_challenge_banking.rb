4720b76a290bef16bc890153a466c5a76b6dc5af
Kok-Hou Chia <kchia87@gmail.com>
2014-09-02 16:51:58 -0700

    Update makersquare_coding_challenge_banking.rb

--- makersquare_coding_challenge_banking.rb
+++ makersquare_coding_challenge_banking.rb
@@ -1,8 +1,9 @@
 class Person
     attr_reader :name
-    def initialize(name,cashInHand)
+    def initialize(name, cashInHand, bankBalance)
         @name = name
         @cashInHand = cashInHand
+        @bankBalance = bankBalance
         puts "Hi, #{@name}. You have $#{@cashInHand}!"
     end
 end
@@ -18,13 +19,23 @@ class Bank
         @customers << customer
         puts "#{customer.name}, thanks for opening an account at #{@bankName}!"
     end
-    def withdraw
-    
+    def withdraw(customer, withAmount)
+        customer.bankBalance -= withAmount
+        customer.cashInHand += withAmount
+        puts "#{customer.name} withdrew #{withAmount} from #{@bankName}. #{customer.name} has #{customer.cashInHand}. #{customer.name}'s account has #{customer.bankBalance}." 
     end
-    def deposit
-    
     end
-    def transfer
-    
+    def deposit(customer, depAmount)
+        if customer.depAmount > customer.cashInHand
+           puts  "#{customer.name}
+        end
+        customer.bankBalance += depAmount
+        customer.cashInHand -= depAmount
+        puts "#{customer.name} deposited #{depAmount} to #{@bankName}. #{customer.name} has #{customer.cashInHand}. #{customer.name}'s account has #{customer.bankBalance}." 
+    end
+    def transfer(customer, transferTo, transAmount)
+        @bankName.customer["bankBalance"] -= transAmount
+        transferTo.customer["bankBalance"] += transAmount
+        puts "#{customer.name} transferred #{transAmount} from the #{@bankName} account to the #{transferTo} account. The #{@bankName} account has #{@bankName.customer["bankBalance"]} and the #{transferTo} account has #{transferTo.customer["bankBalance"]}"
     end
 end
