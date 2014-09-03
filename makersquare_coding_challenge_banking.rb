59d075bd68e95af41048252205eafcf4ac64bf17
Kok-Hou Chia <kchia87@gmail.com>
2014-09-02 17:18:18 -0700

    Update makersquare_coding_challenge_banking.rb

--- makersquare_coding_challenge_banking.rb
+++ makersquare_coding_challenge_banking.rb
@@ -1,9 +1,8 @@
 class Person
     attr_reader :name
-    def initialize(name, cashInHand, bankBalance)
+    def initialize(name, cashInHand)
         @name = name
         @cashInHand = cashInHand
-        @bankBalance = bankBalance
         puts "Hi, #{@name}. You have $#{@cashInHand}!"
     end
 end
@@ -20,22 +19,34 @@ class Bank
         puts "#{customer.name}, thanks for opening an account at #{@bankName}!"
     end
     def withdraw(customer, withAmount)
-        customer.bankBalance -= withAmount
-        customer.cashInHand += withAmount
-        puts "#{customer.name} withdrew #{withAmount} from #{@bankName}. #{customer.name} has #{customer.cashInHand}. #{customer.name}'s account has #{customer.bankBalance}." 
-    end
+        if  withAmount > customer.bankBalance
+            puts "#{customer.name} does not have enough money in the account to withdraw #{withAmount}."
+        else
+            customer.bankBalance -= withAmount
+            customer.cashInHand += withAmount
+            puts "#{customer.name} withdrew #{withAmount} from #{@bankName}. #{customer.name} has #{customer.cashInHand}. #{customer.name}'s account has #{customer.bankBalance}." 
+        end
     end
     def deposit(customer, depAmount)
-        if customer.depAmount > customer.cashInHand
-           puts  "#{customer.name}
+        if depAmount > customer.cashInHand
+           puts  "#{customer.name} does not have enough cash to deposit #{depAmount}."
+        else
+            customer.bankBalance += depAmount
+            customer.cashInHand -= depAmount
+            puts "#{customer.name} deposited #{depAmount} to #{@bankName}. #{customer.name} has #{customer.cashInHand}. #{customer.name}'s account has #{customer.bankBalance}." 
         end
-        customer.bankBalance += depAmount
-        customer.cashInHand -= depAmount
-        puts "#{customer.name} deposited #{depAmount} to #{@bankName}. #{customer.name} has #{customer.cashInHand}. #{customer.name}'s account has #{customer.bankBalance}." 
     end
     def transfer(customer, transferTo, transAmount)
         @bankName.customer["bankBalance"] -= transAmount
         transferTo.customer["bankBalance"] += transAmount
         puts "#{customer.name} transferred #{transAmount} from the #{@bankName} account to the #{transferTo} account. The #{@bankName} account has #{@bankName.customer["bankBalance"]} and the #{transferTo} account has #{transferTo.customer["bankBalance"]}"
+    end
+    def total_cash_in_bank
+        total = 0
+        i = 0
+        while i < @customers.count
+            total = total + @customers[i].bankBalance
+        end
+        puts "#{@bankName} has #{total} in the bank."
     end
 end
