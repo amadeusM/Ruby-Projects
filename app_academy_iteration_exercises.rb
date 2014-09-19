a65125756ef84daffed31cee37da9aac1bf9551a
Kok-Hou Chia <kchia87@gmail.com>
2014-09-19 12:44:55 -0700

    Create app_academy_iteration_exercises.rb

---
+++ app_academy_iteration_exercises.rb
@@ -0,0 +1,11 @@
+#Loop through all the numbers, stopping when you encounter a number that is (a) >250 and (b) divisible by 7. Print this number down!
+i = 251
+while i % 7 != 0 
+    i += 1
+end
+print i
+
+#Print out all the factors for each of the numbers 1 through 100.
+
+
+#Write a method substrings that will take a String and return an array containing each of its substrings. Example output: substrings("cat") => ["c", "ca", "cat", "a", "at", "t"]
